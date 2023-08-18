1966f367d08305a1c445e9bdb9db00b115261a4f
LucasCruz <lca.snf21@uea.edu.br>
2023-08-18 00:44:41 -0400

    create views

--- asvagas/app/views.py
+++ asvagas/app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 # Create your views here.
+from .forms import *
 
 def index(request):
     return render(request, "templates/index.html")
+
+def login(request):
+    return render(request, "templates/login.html")
+
+def register(request):
+    form_candidate = RegistrationFormCandidate()
+    form_recruiter = RegistrationFormRecruiter()
+    return render(request, "templates/register.html",{'form_candidate':form_candidate, 'form_recruiter':form_recruiter})
